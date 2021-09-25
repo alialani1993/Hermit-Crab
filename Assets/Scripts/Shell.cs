0fbcbbd697c48daf8be22a733a030d89186334f4
Ali Al-Ani <aliahmadalani@gmail.com>
2021-09-24 22:34:45 -0500

    SmoothCam, Tutorial level progress

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -7,6 +7,9 @@ public class Shell : MonoBehaviour
     public bool shellPickUp;
     public Transform shellHover;
     public Transform currentShell;
+    public BoxCollider2D Collider;
+    public BoxCollider2D frictionlessCollider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,16 +27,20 @@ public class Shell : MonoBehaviour
                 shellHover.SetParent(transform);
                 shellHover.transform.position = transform.position + new Vector3(0, 1, 0);
                 currentShell = shellHover;
-                transform.GetComponent<BoxCollider2D>().size = new Vector2(1,2);
-                transform.GetComponent<BoxCollider2D>().offset = new Vector2(0,0.5f);
+                Collider.size = new Vector2(1,1.5f);
+                Collider.offset = new Vector2(0,0.3f);
+                frictionlessCollider.size = new Vector2(1.3f,1.5f);
+                frictionlessCollider.offset = new Vector2(0, 0.3f);
                 shellHover = null;
 
             }
             else {
                 transform.GetChild(0).SetParent(null);
                 currentShell = null;
-                transform.GetComponent<BoxCollider2D>().size = new Vector2(1, 1);
-                transform.GetComponent<BoxCollider2D>().offset = Vector2.zero;
+                Collider.size = new Vector2(1, 0.7f);
+                Collider.offset = new Vector2(0, -0.1f);
+                frictionlessCollider.size = new Vector2(1.3f, 0.75f);
+                frictionlessCollider.offset = new Vector2(0, -0.075f);
 
             }
             }
